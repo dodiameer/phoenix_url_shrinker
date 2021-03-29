181e2b26c0932ab05b6716793f2c010c7fc0c3cd
dodiameer <mtxshiftg@gmail.com>
2021-03-29 19:59:24 +0300

    docs: Added docs to `link_view.ex`

--- lib/url_shrinker_web/views/link_view.ex
+++ lib/url_shrinker_web/views/link_view.ex
@@ -1,16 +1,16 @@
 defmodule UrlShrinkerWeb.LinkView do
   use UrlShrinkerWeb, :view
-  # alias UrlShrinkerWeb.Router
-  # alias UrlShrinkerWeb.LinkView
 
-  # def render("index.json", %{links: links}) do
-  #   %{data: render_many(links, LinkView, "link.json")}
-  # end
+  @doc """
+  Link info renderer
 
-  # def render("redirect.json", %{link: link}) do
-  #   %{data: render_one(link, LinkView, "link.json")}
-  # end
-
+  ## Return data
+  ```json
+  {
+    "link": "<Short link url>"
+  }
+  ```
+  """
   def render("info.json", %{short_url: link}) do
     %{link: link}
   end
